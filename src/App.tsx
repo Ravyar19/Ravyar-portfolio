2625f82e4b3e257236d3a1af65871eb8325511ee
Ravyar19 <ravyar.ara@gmail.com>
2024-03-23 21:24:02 +0300

    animated cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Contact from './components/contact/Contact';
+import Cursor from './components/cursor/Cursor';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
@@ -8,6 +9,8 @@ import Services from './components/services/Services';
 function App() {
   return (
     <div>
+      <Cursor />
+
       <section id='Homepage'>
         <Navbar />
         <Hero />
